895712798a3ce6e10cb4a45318bb6bb8596f40f9
Ihor Salnikov <ihor.salnikov@gmail.com>
2016-11-22 17:47:00 +0100

    ths not working

--- src/main/kotlin/io/neuro/vort/App.kt
+++ src/main/kotlin/io/neuro/vort/App.kt
@@ -1,17 +1,17 @@
 package io.neuro.vort
 
 import java.awt.Color
+import java.awt.Point
 import java.awt.image.BufferedImage
-import java.awt.image.WritableRaster
+import java.awt.image.DataBufferByte
+import java.awt.image.DataBufferInt
+import java.awt.image.Raster
 import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.GrayFilter
 
-class App {
 
-}
-
-val path = "C:/Users/isalnikov/Desktop/VORT/src/main/resources"
+val path = "./src/main/resources"
 
 
 fun main(args: Array<String>) {
@@ -20,36 +20,30 @@ fun main(args: Array<String>) {
     val fastGRB = FastGRB(bufferedImage!!)
 
     val start: Long = System.currentTimeMillis()
-    val rgb = fastGRB.getRGB(1, 1)
+    val rgb = fastGRB.getRGB(0, 0)
     val color = fastGRB.getColor(rgb!!)
     val end = System.currentTimeMillis()
 
     val pixels: ByteArray? = fastGRB.pixels
 
-    var pix = ByteArray(fastGRB.height* fastGRB.width)
+    val pix = IntArray(fastGRB.height * fastGRB.width)
 
-    val grayFilter = GrayFilter(false, 50)
+    val grayFilter = GrayFilter(true, 50)
 
     for (x: Int in 0..fastGRB.width - 1) {
         for (y: Int in 0..fastGRB.height - 1) {
-            val filterRGB = grayFilter.filterRGB(0, 0, fastGRB.getRGB(x, y)!!)
-
+            pix[(x * y)] = grayscale(fastGRB.getRGB(x, y)!!)
         }
     }
-//    val image = BufferedImage(fastGRB.width, fastGRB.height, BufferedImage.TYPE_INT_ARGB)
-//    val data = image.data as WritableRaster
-//    data.setPixels(0,0,fastGRB.width,fastGRB.height,)
 
-    println(rgb)
-    println(end - start)
+    val image = BufferedImage(fastGRB.width, fastGRB.height, BufferedImage.TYPE_INT_ARGB)
+    val image2 = BufferedImage(fastGRB.width,fastGRB.height, BufferedImage.TYPE_4BYTE_ABGR)
+    image2.data = Raster.createRaster(image2.sampleModel,DataBufferByte(fastGRB.pixels!!, fastGRB.pixels.size), Point())
+    image.data = Raster.createRaster(image.sampleModel, DataBufferInt(pix, pix.size+1), Point())
+    ImageIO.write(image, "PNG", File(path, "kurwa.png"))
+    ImageIO.write(image2,"PNG", File(path, "pls.png"))
+    println(end-start)
 
-    //val start: Long = System.currentTimeMillis()
-    //val image = grayscale(bufferedImage)
-    //val end = System.currentTimeMillis()
-//
-    //println(end - start)
-
-    //ImageIO.write(image, "PNG", File(path + start.toString() + ".png"))
 }
 
 fun lense(size: Int): BufferedImage {
@@ -85,27 +79,11 @@ fun xor(size: Int): BufferedImage {
     return image
 }
 
-fun grayscale(image: BufferedImage?): BufferedImage? {
-    val raster = image!!.raster
-    val height = raster.height
-    val width = raster.width
+fun grayscale(rgb: Int): Int {
+    val c = Color(rgb)
+    val red = (c.getRed() * 0.299).toInt()
+    val green = (c.getGreen() * 0.587).toInt()
+    val blue = (c.getBlue() * 0.114).toInt()
 
-    val fastRGB = FastRGB(image)
-
-    var array = IntArray(height * width)
-
-    for (x: Int in 0..width - 1) {
-        for (y: Int in 0..height - 1) {
-            val rgb: Int = fastRGB.getRGB(x, y)
-
-
-//            val red = .getRed() * 0.299
-//            val green = c.getGreen() * 0.587
-//            val blue = c.getBlue() * 0.114
-
-        }
-    }
-    return null
-
-
+    return Color(red, green,blue,255).rgb
 }
--- src/main/kotlin/io/neuro/vort/FastGRB.kt
+++ src/main/kotlin/io/neuro/vort/FastGRB.kt
@@ -7,34 +7,36 @@ import java.awt.image.DataBufferByte
 class FastGRB(image: BufferedImage) {
 
     val pixels: ByteArray? = (image.raster.dataBuffer as DataBufferByte).data
-    val height = image.height
-    val width = image.width
-    val alfa = if (image.alphaRaster != null) true else false
+    val height = image.height - 1
+    val width = image.width - 1
+    val alfa: Boolean = if (image.alphaRaster != null) true else false
     val pixelLength = if (alfa) 4 else 3
 
 
     fun getRGB(x: Int, y: Int): Int? {
+
         var pixel: Int = getPixel(x, y)
 
-        var argb: Int = -16777216
+        var argb: Int = 0
+        argb += -16777216
 
         if (alfa) {
             argb += (pixels?.get(pixel++)?.toInt()?.and(0xff))?.shl(24)!!
         }
-        argb += (pixels?.get(pixel++)?.toInt()?.and(0xFF))?.shl(16)!!//red
-        argb += (pixels?.get(pixel++)?.toInt()?.and(0xFF))?.shl(8)!! //green
-        argb += (pixels?.get(pixel++)?.toInt()?.and(0xFF))!! //blue
+
+        val blue: Int = (pixels?.get(pixel++)?.toInt()?.and(0xFF))?.shl(0)!!//blue
+        val green: Int = (pixels?.get(pixel++)?.toInt()?.and(0xFF))?.shl(8)!!//green
+        val red: Int = (pixels?.get(pixel++)?.toInt()?.and(0xFF))?.shl(16)!!//red
+
+        argb += green
+        argb += red
+        argb += blue
 
         return argb
     }
 
-    fun getColor(grb: Int): Color {
-        val alfa = grb.shr(24).and(0xFF)
-        val red = grb.shr(16).and(0xFF)
-        val green = grb.shr(8).and(0xFF)
-        val blue = grb.shr(0).and(0xFF)
-        val color = Color(red, green, blue, alfa)
-        Color(3)
+    fun getColor(rgb: Int): Color {
+        val color = Color(rgb, alfa)
         return color
     }
 
@@ -44,6 +46,6 @@ class FastGRB(image: BufferedImage) {
 
     }
 
-    private fun getPixel(x: Int, y: Int) = (y * pixelLength * width) + (x * pixelLength)
+    private fun getPixel(x: Int, y: Int) = pixelLength * (x + y * width)
 
 }
