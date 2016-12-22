59360aebd6c47807cfe81e91abcff1db71c42015
Ihor <ihor.salnikov@gmail.com>
2016-12-22 16:57:54 +0100

    working on math block

--- src/main/java/com/metjka/vort/ui/components/blocks/MathBlock.java
+++ src/main/java/com/metjka/vort/ui/components/blocks/MathBlock.java
@@ -1,19 +1,28 @@
 package com.metjka.vort.ui.components.blocks;
 
 import com.metjka.vort.ui.ToplevelPane;
+import com.metjka.vort.ui.Type;
 import com.metjka.vort.ui.components.connections.InputAnchor;
 import com.metjka.vort.ui.components.connections.OutputAnchor;
 
 import java.util.List;
 import java.util.Optional;
 
-public class MathBlock extends Block {
-    /**
-     * @param pane The pane this block belongs to.
-     */
+public class MathBlock extends Block implements TwoOutputBlock<Integer> {
+
+    Integer firstValue;
+    Integer secondValue;
+
+    InputAnchor inputAnchor1;
+    InputAnchor inputAnchor2;
+
+    OutputAnchor outputAnchor;
+
     public MathBlock(ToplevelPane pane) {
         super(pane);
         loadFXML("MathBlock");
+        inputAnchor1 = new InputAnchor(this, Type.NUMBER);
+
     }
 
     @Override
@@ -26,11 +35,6 @@ public class MathBlock extends Block {
         return null;
     }
 
-    protected Integer getValue() {
-
-        return null;
-    }
-
     @Override
     public void invalidateVisualState() {
 
@@ -39,5 +43,15 @@ public class MathBlock extends Block {
     @Override
     public Optional<Block> getNewCopy() {
         return null;
+    }
+
+    @Override
+    public Integer getFirstValue() {
+        return firstValue;
+    }
+
+    @Override
+    public Integer getSecondValue() {
+        return secondValue;
     }
 }
